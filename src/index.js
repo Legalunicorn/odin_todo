a60dcc09cfabe590e75ad156ba608fbf1eaa5088
Nathan Yeo <nyeo700@gmail.com>
2024-01-25 16:19:18 +0800

    completed modal idea input styling and checkbox logic

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 console.log(' hoo')
+
+//this module is to run when the page is opened
+
+import client from "/modules/client"
+
+client.loadProjects();
---
+++ src/modules/client.js
@@ -0,0 +1,28 @@
+//this is for the buttons to do stuff when pressed
+//load all the stuff too
+
+//import from storage to load stuff
+
+
+
+const client = ()=>{
+    function loadProjects(){
+        console.log('FATTY BOMBBOMB')
+    }
+
+    function loadTask(projectName){
+        //use storage to 
+    }
+
+    function addTask(){
+
+    }
+
+    function addButton(){
+
+    }
+
+    return {loadProjects}
+}
+
+export default client;
